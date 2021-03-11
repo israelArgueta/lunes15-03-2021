6893fdcbb8bf56c1114171e563c5287afc5c6566
israel argueta <israelargueta@mesoamericana.edu.gt>
2021-03-11 12:49:26 -0600

    nueva clase coutas

---
+++ lunes15-03-2021/coutas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lunes15_03_2021
+{
+    class coutas
+    {
+        string perso;
+        string ap;
+        double man;
+
+        public string Perso { get => perso; set => perso = value; }
+        public string Ap { get => ap; set => ap = value; }
+        public double Man { get => man; set => man = value; }
+    }
+}
+
